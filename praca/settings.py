d5061e17683d35e2a6f5c0d4865d65940d51d5a7
Tomasz Czuba <t.czuba95@gmail.com>
2020-11-04 18:08:29 +0100

    Update z 04.11.2020 settings

--- praca/settings.py
+++ praca/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'a68*%(%v8h2!pr$lpoorqd%(-wyf!b3c2@1dk6+nc=&r&y4&f-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1","tomasz-czuba-praca-dyplomowa.herokuapp.com"]
 
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
